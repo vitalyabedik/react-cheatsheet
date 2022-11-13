2de6808cf186d788f6003bcfe2b7251244e15c78
Vitali Bedik <vitalya.bedik@gmail.com>
2022-11-13 20:29:23 +0300

    adding data to VirtualDom and Home pages

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,16 @@
+import { Typography, Image } from 'antd';
+
+import courseProgram from '../../assets/images/courseProgram.png';
+
+const { Title } = Typography;
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <>
+            <Title level={2}>Программа курса Redev</Title>
+            <Image src={courseProgram} />
+        </>
+    );
 };
 
 export default Home;
