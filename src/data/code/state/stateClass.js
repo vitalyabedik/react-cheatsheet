321a734546319c672b2e607627779f37a952ce1d
Vitali Bedik <vitalya.bedik@gmail.com>
2022-12-09 00:38:10 +0300

    adding state data and pages

--- src/data/code/state/stateClass.js
+++ src/data/code/state/stateClass.js
@@ -1,1 +1,51 @@
-export const stateClass = [];
+export const stateClass = {
+    stateWithConstructor: `class Test extends React.Component {
+constructor(props) {
+    super(props);
+    this.state = {
+        count: 0,
+    };
+}
+
+render() {
+    return (
+        <div>
+            <p>Counter example</p>
+        </div>
+        );
+    }
+}    
+`,
+    stateWithoutConstructor: `class Test extends React.Component {
+    state = {
+        count: 0  
+    };
+    
+    render() {
+        return (
+            <div>
+                <p>Counter example</p>
+            </div>
+            );
+        }
+    }    
+    `,
+    stateRender: `class Test extends React.Component {
+    state = {
+        count: 0,
+    };
+
+    render() {
+        return <div>{this.state.count}</div>;
+    }
+}`,
+    stateFirstParamObj: `<button onClick={() => this.setState((state) => ({ count: 5 }))}>
+Click1
+  </button>`,
+    stateFirstParamFunc: `<button onClick={() => this.setState((state) => ({ count: this.state.count + 1 }))}>
+Click2
+  </button>`,
+    stateSecondParam: `<button onClick={() => this.setState({ count: this.state.count + 1 }, () => console.log(this.state))}>
+Click2
+  </button>`,
+};
--- src/data/code/state/stateFunctional.js
+++ src/data/code/state/stateFunctional.js
@@ -1,1 +1,24 @@
-export const stateFunctional = [];
+export const stateFunctional = {
+    import: `import { useState } from 'react'`,
+    vars: `const state = useState()`,
+    state: `const [state, setState] = useState()`,
+    count: `const [count, setCount] = useState()`,
+    startValue: `const [count, setCount] = useState(0)`,
+    render: `const Test = () => {
+    const [count, setCount] = useState(0);
+      
+    return <div>{count}</div>;
+};`,
+    counter: `const Test = () => {
+    const [count, setCount] = useState(0);
+      
+    return (
+        <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>
+              Click
+            </button>
+        </div>
+    )
+}`,
+};
