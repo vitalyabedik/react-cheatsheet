5cd5bd269d80fa67d77893849e4b8dae141d419b
Vitali Bedik <vitalya.bedik@gmail.com>
2022-12-16 20:10:57 +0300

    add data code to useEffect

--- src/data/code/useEffect/useEffect.js
+++ src/data/code/useEffect/useEffect.js
@@ -1,1 +1,10 @@
-export const useEffect = [];
+export const useEffect = {
+    basic: `useEffect(() => {}, [])`,
+    onlyFirstParam: `useEffect(() => { console.log('-componentDidMount-')})`,
+    oneRender: `useEffect(() => { console.log('-componentDidMount-')} , [])`,
+    update: `useEffect(() => { console.log('-componentDidUpdate-') }, [count])`,
+    unMount: `useEffect(() => {
+        console.log("-componentDidMount-");
+        return () => console.log("-componentWillUnmount-");
+      }, []);`,
+};
