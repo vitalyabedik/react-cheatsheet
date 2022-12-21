c10f6fce02d0edd0f2ccd88b3d2a5ce9f7768568
Vitali Bedik <vitalya.bedik@gmail.com>
2022-12-21 20:27:11 +0300

    add code data Fragment

--- src/data/code/fragment/fragment.js
+++ src/data/code/fragment/fragment.js
@@ -1,1 +1,28 @@
-export const fragment = [];
+export const fragment = {
+  short: `const Fragment = () => {
+      return (
+          <>
+              <h1>Vitalya</h1>
+              <h3>I am learning React</h3>
+              <button>Like</button>
+          </>
+      );
+  };
+  
+  export default Fragment;
+  `,
+  long: `import React from 'react';
+
+const Fragment = () => {
+    return (
+        <React.Fragment>
+            <h1>Vitalya</h1>
+            <h3>I am learning React</h3>
+            <button>Like</button>
+        </React.Fragment>
+    );
+};
+  
+  export default Fragment;
+  `,
+};
