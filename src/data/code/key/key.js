c57e3efa1eb2c799c6072245612b136c7b68ee57
Vitali Bedik <vitalya.bedik@gmail.com>
2022-12-23 00:12:06 +0300

    add code data key

--- src/data/code/key/key.js
+++ src/data/code/key/key.js
@@ -1,1 +1,22 @@
-export const key = [];
+export const key = {
+  example: `const Test = () => {
+    const texts = [
+      'Click me',
+      'Click me please',
+      'Hit me',
+      'Press me',
+      'Click me again!, Press me!!!',
+  ];
+  
+    return (
+      <>
+        {texts.map(item => (
+          <p key={item}>{item}</p>
+        ))}
+      </>
+    );
+  };
+  
+  export default Test;
+`,
+};
