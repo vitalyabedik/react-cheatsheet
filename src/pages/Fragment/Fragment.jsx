e23b763896e45b0039b604b90025f76fe072324f
Vitali Bedik <vitalya.bedik@gmail.com>
2022-12-22 19:36:18 +0300

    add data page Fragment

--- src/pages/Fragment/Fragment.jsx
+++ src/pages/Fragment/Fragment.jsx
@@ -1,5 +1,78 @@
+import { Typography, Divider } from 'antd';
+
+import CodeBlock from '../../components/CodeBlock/CodeBlock';
+import { fragment } from '../../data';
+
+const { Title, Text, Paragraph } = Typography;
+
 const Fragment = () => {
-    return <div>Fragment</div>;
+  return (
+    <>
+      <Title level={2}>Fragment</Title>
+
+      <Divider>Что такое Fragment? </Divider>
+      <Paragraph>
+        <Text keyboard>Fragment</Text> - это как не названный тег, которого не
+        существует.
+      </Paragraph>
+      <Paragraph>
+        <Text keyboard>Fragment не отображается в DOM.</Text>
+      </Paragraph>
+
+      <Divider>Для чего нужен Fragment?</Divider>
+      <Paragraph>
+        Fragment нужен для render (рендера), т.к. наша компонента должна
+        возвращать что-то одно.
+      </Paragraph>
+
+      <Divider>Преимущества использования Fragment?</Divider>
+      <Paragraph>
+        <ol>
+          <li>
+            {' '}
+            <Paragraph>
+              Уменьшение размера реального DOM (нету ненужного div в разметке).
+            </Paragraph>
+          </li>
+          <li>
+            {' '}
+            <Paragraph>
+              Оптимизирует процесс обновления реального DOM в процессе ЖЦ
+              приложения.
+            </Paragraph>
+          </li>
+        </ol>
+      </Paragraph>
+
+      <Divider>Способы записи Fragment?</Divider>
+      <Paragraph>
+        <ol>
+          <li>
+            {' '}
+            <Paragraph>
+              <Text keyboard>Сокращенная запись:</Text>
+            </Paragraph>
+          </li>
+          <CodeBlock code={fragment.short} />
+          <Paragraph>
+            При данной записи нет возможности его стилизовать (задавать
+            атрибуты).
+          </Paragraph>
+          <li>
+            {' '}
+            <Paragraph>
+              <Text keyboard>Полная запись:</Text>
+            </Paragraph>
+          </li>
+          <CodeBlock code={fragment.long} />
+        </ol>
+      </Paragraph>
+      <Paragraph>
+        При данной записи появился импорт из библиотеки React, но данный способ
+        позволяет стилизовать фрагмент (задавать атрибуты).
+      </Paragraph>
+    </>
+  );
 };
 
 export default Fragment;
--- src/pages/State/StateClass.jsx
+++ src/pages/State/StateClass.jsx
@@ -6,87 +6,86 @@ import { stateClass } from '../../data';
 const { Title, Text, Paragraph } = Typography;
 
 const ClassState = () => {
-    return (
-        <>
-            <Title level={2}>State. Классовые компоненты</Title>
+  return (
+    <>
+      <Title level={2}>State. Классовые компоненты</Title>
 
-            <Divider>Для чего нужен state?</Divider>
-            <Paragraph>
-                <Text keyboard>State</Text> - переменная которую нужно изменять
-                и которая является объектом.
-            </Paragraph>
+      <Divider>Для чего нужен state?</Divider>
+      <Paragraph>
+        <Text keyboard>State</Text> - переменная которую нужно изменять и
+        которая является объектом.
+      </Paragraph>
 
-            <Divider>Способы объявления state?</Divider>
-            <Paragraph>
-                <ol>
-                    <li>
-                        <Text keyboard>С помощью конструктора</Text> - очень
-                        долго, т.к. нужно написать constructor, super и тд.
-                    </li>
-                    <CodeBlock code={stateClass.stateWithConstructor} />
-                    <li>
-                        <Text keyboard>Без конструктора:</Text>{' '}
-                        <CodeBlock code={stateClass.stateWithoutConstructor} />
-                    </li>
-                </ol>
-            </Paragraph>
+      <Divider>Способы объявления state?</Divider>
+      <Paragraph>
+        <ol>
+          <li>
+            <Text keyboard>С помощью конструктора</Text> - очень долго, т.к.
+            нужно написать constructor, super и тд.
+          </li>
+          <CodeBlock code={stateClass.stateWithConstructor} />
+          <li>
+            <Text keyboard>Без конструктора:</Text>{' '}
+            <CodeBlock code={stateClass.stateWithoutConstructor} />
+          </li>
+        </ol>
+      </Paragraph>
 
-            <Divider>Как вывести state в разметку?</Divider>
-            <Paragraph>
-                Для этого используем <Text keyboard>this.state</Text>:
-            </Paragraph>
-            <CodeBlock code={stateClass.stateRender} />
+      <Divider>Как вывести state в разметку?</Divider>
+      <Paragraph>
+        Для этого используем <Text keyboard>this.state</Text>:
+      </Paragraph>
+      <CodeBlock code={stateClass.stateRender} />
 
-            <Divider>Как изменять state?</Divider>
-            <Paragraph>
-                Для этого существует специальный метод
-                <Text keyboard>setState</Text>
-            </Paragraph>
+      <Divider>Как изменять state?</Divider>
+      <Paragraph>
+        Для этого существует специальный метод
+        <Text keyboard>setState</Text>
+      </Paragraph>
 
-            <Divider>Какие параметры у setState?</Divider>
-            <Paragraph>
-                <ol>
-                    <li>Первый параметр может быть объектом либо функцией:</li>
-                    <ul>
-                        <li>
-                            <Text keyboard>Объект:</Text>
-                        </li>
-                        <CodeBlock code={stateClass.stateFirstParamObj} />
-                        <li>
-                            <Text keyboard>Функция:</Text>
-                            <CodeBlock code={stateClass.stateFirstParamFunc} />
-                        </li>
-                    </ul>
+      <Divider>Какие параметры у setState?</Divider>
+      <Paragraph>
+        <ol>
+          <li>Первый параметр может быть объектом либо функцией:</li>
+          <ul>
+            <li>
+              <Text keyboard>Объект:</Text>
+            </li>
+            <CodeBlock code={stateClass.stateFirstParamObj} />
+            <li>
+              <Text keyboard>Функция:</Text>
+              <CodeBlock code={stateClass.stateFirstParamFunc} />
+            </li>
+          </ul>
 
-                    <li>
-                        Второй параметр - callback-функция, которая сработает,
-                        когда state обновится:
-                    </li>
-                    <Paragraph>
-                        <Text keyboard>
-                            Иногда может быть полезен, когда у нас есть какое-то
-                            значение и затем это измененное значение нужно
-                            отправить на сервер.
-                        </Text>
-                    </Paragraph>
-                </ol>
-            </Paragraph>
-            <CodeBlock code={stateClass.stateSecondParam} />
+          <li>
+            Второй параметр - callback-функция, которая сработает, когда state
+            обновится:
+          </li>
+          <Paragraph>
+            <Text keyboard>
+              Иногда может быть полезен, когда у нас есть какое-то значение и
+              затем это измененное значение нужно отправить на сервер.
+            </Text>
+          </Paragraph>
+        </ol>
+      </Paragraph>
+      <CodeBlock code={stateClass.stateSecondParam} />
 
-            <Divider>Что нельзя делать с методом setState?</Divider>
-            <Paragraph>
-                Его нельзя вызывать в методе render, иначе будет ошибка, т.к.
-                произойдет зацикливание.
-            </Paragraph>
-            <Paragraph>
-                Это происходит по той причине, что каждый вызов setState
-                вызывает метод render из-за чего происходит rerender
-            </Paragraph>
-            <Paragraph>
-                <Text keyboard>Вызов setState является асинхронным</Text>
-            </Paragraph>
-        </>
-    );
+      <Divider>Что нельзя делать с методом setState?</Divider>
+      <Paragraph>
+        Его нельзя вызывать в методе render, иначе будет ошибка, т.к. произойдет
+        зацикливание.
+      </Paragraph>
+      <Paragraph>
+        Это происходит по той причине, что каждый вызов setState вызывает метод
+        render из-за чего происходит rerender
+      </Paragraph>
+      <Paragraph>
+        <Text keyboard>Вызов setState является асинхронным</Text>
+      </Paragraph>
+    </>
+  );
 };
 
 export default ClassState;
