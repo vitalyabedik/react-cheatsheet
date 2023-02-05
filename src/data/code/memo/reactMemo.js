c1afd43d08aa619f49a6eb5fd43403e17bd01ffe
Vitali Bedik <vitalya.bedik@gmail.com>
2023-02-05 09:25:50 +0300

    add code for useMemo, React.memo

--- src/data/code/memo/reactMemo.js
+++ src/data/code/memo/reactMemo.js
@@ -1,1 +1,46 @@
-export const reactMemo = [];
+export const reactMemo = {
+  reactMemoBasic: `const MyComponent = React.memo(function MyComponent(props) {
+    /* повторный рендеринг данного компонента выполняется только при изменении передаваемых ему пропcов */
+  })`,
+  reactMemoBasicExample: `import React from 'react';
+
+  const MyComponent = React.memo((props) => {
+    return (
+      <div>
+        {props.data}
+      </div>
+    );
+  });
+  
+  export default MyComponent;`,
+  reactMemoExampleShouldComponentUpdate: `import React from 'react';
+
+  const MyComponent = React.memo((props) => {
+    return (
+      <div>
+        <p>props: {props.data}</p>
+      </div>
+    );
+  }, (prevProps, nextProps) => {
+    return prevProps.data === nextProps.data;
+  });
+  
+  export default MyComponent;`,
+  reactMemoExampleShouldComponentUpdateFalse: `import React from 'react';
+
+  const MyComponent = React.memo((props) => {
+    return (
+      <div>
+        <p>props: {props.data}</p>
+      </div>
+    );
+  }, (prevProps, nextProps) => {
+    if (nextProps.data === 4) {
+      return false
+    } else {
+      return true
+    }
+  });
+  
+  export default MyComponent;`,
+};
--- src/data/code/memo/useMemo.js
+++ src/data/code/memo/useMemo.js
@@ -1,1 +1,25 @@
-export const useMemo = [];
+export const useMemo = {
+  basic: `useMemo(() => {
+    fn,
+    [deps]
+  }) // deps - зависимости
+  
+  /*============или присвоить результат============*/
+
+  const result = useMemo(linkFn, [deps]) 
+  `,
+  example: `import React, { useMemo } from 'react';
+
+  function MyComponent({ data }) {
+    const memoizedValue = useMemo(() => {
+      // вычисление какого-то значения
+      return heavyComputation(data);
+    }, [data]);
+  
+    return (
+      <div>
+        {memoizedValue}
+      </div>
+    );
+  }`,
+};
