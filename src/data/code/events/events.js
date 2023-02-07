932789ae73448457cbe61ee1bf2b65f9e00b8c6e
Vitali Bedik <vitalya.bedik@gmail.com>
2023-02-07 22:41:08 +0300

    add code for events page

--- src/data/code/events/events.js
+++ src/data/code/events/events.js
@@ -1,1 +1,36 @@
-export const events = [];
+export const events = {
+  basic: `<button onClick={handleClick}>Click me</button>`,
+  onClick: `import React from "react";
+
+  const ExampleComponent = () => {
+    const handleClick = () => {
+      console.log("Button was clicked");
+    };
+  
+    return (
+      <button onClick={handleClick}>
+        Click me
+      </button>
+    );
+  };
+  
+  export default ExampleComponent;`,
+  onChange: `import React, { useState } from 'react';
+
+  const Example = () => {
+    const [inputValue, setInputValue] = useState('');
+  
+    const handleChange = (event) => {
+      setInputValue(event.target.value);
+    };
+  
+    return (
+      <div>
+        <input type="text" value={inputValue} onChange={handleChange} />
+        <p>Input value: {inputValue}</p>
+      </div>
+    );
+  };
+  
+  export default Example;`,
+};
