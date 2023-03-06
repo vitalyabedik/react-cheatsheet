1686dadb18b714f4677825a3f0d43add8ba3ea9d
Vitali Bedik <vitalya.bedik@gmail.com>
2023-03-06 20:26:41 +0300

    remove dividers and add titles to page StateClass

--- src/pages/State/StateClass.jsx
+++ src/pages/State/StateClass.jsx
@@ -1,4 +1,4 @@
-import { Typography, Divider } from 'antd';
+import { Typography } from 'antd';
 
 import CodeBlock from '../../components/CodeBlock/CodeBlock';
 import { stateClass } from '../../data';
@@ -10,18 +10,17 @@ const ClassState = () => {
     <>
       <Title level={2}>State. Классовые компоненты</Title>
 
-      <Divider>Для чего нужен state?</Divider>
+      <Title level={3}>Для чего нужен state?</Title>
       <Paragraph>
-        <Text keyboard>State</Text> - переменная которую нужно изменять и
-        которая является объектом.
+        <Text keyboard>State</Text> - переменная которую нужно изменять и которая является объектом.
       </Paragraph>
 
-      <Divider>Способы объявления state?</Divider>
+      <Title level={3}>Способы объявления state?</Title>
       <Paragraph>
         <ol>
           <li>
-            <Text keyboard>С помощью конструктора</Text> - очень долго, т.к.
-            нужно написать constructor, super и тд.
+            <Text keyboard>С помощью конструктора</Text> - очень долго, т.к. нужно написать
+            constructor, super и тд.
           </li>
           <CodeBlock code={stateClass.stateWithConstructor} />
           <li>
@@ -31,19 +30,19 @@ const ClassState = () => {
         </ol>
       </Paragraph>
 
-      <Divider>Как вывести state в разметку?</Divider>
+      <Title level={3}>Как вывести state в разметку?</Title>
       <Paragraph>
         Для этого используем <Text keyboard>this.state</Text>:
       </Paragraph>
       <CodeBlock code={stateClass.stateRender} />
 
-      <Divider>Как изменять state?</Divider>
+      <Title level={3}>Как изменять state?</Title>
       <Paragraph>
         Для этого существует специальный метод
         <Text keyboard>setState</Text>
       </Paragraph>
 
-      <Divider>Какие параметры у setState?</Divider>
+      <Title level={3}>Какие параметры у setState?</Title>
       <Paragraph>
         <ol>
           <li>Первый параметр может быть объектом либо функцией:</li>
@@ -58,28 +57,24 @@ const ClassState = () => {
             </li>
           </ul>
 
-          <li>
-            Второй параметр - callback-функция, которая сработает, когда state
-            обновится:
-          </li>
+          <li>Второй параметр - callback-функция, которая сработает, когда state обновится:</li>
           <Paragraph>
             <Text keyboard>
-              Иногда может быть полезен, когда у нас есть какое-то значение и
-              затем это измененное значение нужно отправить на сервер.
+              Иногда может быть полезен, когда у нас есть какое-то значение и затем это измененное
+              значение нужно отправить на сервер.
             </Text>
           </Paragraph>
         </ol>
       </Paragraph>
       <CodeBlock code={stateClass.stateSecondParam} />
 
-      <Divider>Что нельзя делать с методом setState?</Divider>
+      <Title level={3}>Что нельзя делать с методом setState?</Title>
       <Paragraph>
-        Его нельзя вызывать в методе render, иначе будет ошибка, т.к. произойдет
-        зацикливание.
+        Его нельзя вызывать в методе render, иначе будет ошибка, т.к. произойдет зацикливание.
       </Paragraph>
       <Paragraph>
-        Это происходит по той причине, что каждый вызов setState вызывает метод
-        render из-за чего происходит rerender
+        Это происходит по той причине, что каждый вызов setState вызывает метод render из-за чего
+        происходит rerender
       </Paragraph>
       <Paragraph>
         <Text keyboard>Вызов setState является асинхронным</Text>
