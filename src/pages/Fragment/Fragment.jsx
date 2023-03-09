ee079b94e25bf6f4c6e65b39dcacaf464c7c30c2
Vitali Bedik <vitalya.bedik@gmail.com>
2023-03-09 20:50:12 +0300

    remove dividers and add title to page Fragment

--- src/pages/Fragment/Fragment.jsx
+++ src/pages/Fragment/Fragment.jsx
@@ -1,4 +1,4 @@
-import { Typography, Divider } from 'antd';
+import { Typography } from 'antd';
 
 import CodeBlock from '../../components/CodeBlock/CodeBlock';
 import { fragment } from '../../data';
@@ -10,41 +10,36 @@ const Fragment = () => {
     <>
       <Title level={2}>Fragment</Title>
 
-      <Divider>Что такое Fragment? </Divider>
+      <Title level={3}>Что такое Fragment? </Title>
       <Paragraph>
-        <Text keyboard>Fragment</Text> - это как не названный тег, которого не
-        существует.
+        <Text keyboard>Fragment</Text> - это как не названный тег, которого не существует.
       </Paragraph>
       <Paragraph>
         <Text keyboard>Fragment не отображается в DOM.</Text>
       </Paragraph>
 
-      <Divider>Для чего нужен Fragment?</Divider>
+      <Title level={3}>Для чего нужен Fragment?</Title>
       <Paragraph>
-        Fragment нужен для render (рендера), т.к. наша компонента должна
-        возвращать что-то одно.
+        Fragment нужен для render (рендера), т.к. наша компонента должна возвращать что-то одно.
       </Paragraph>
 
-      <Divider>Преимущества использования Fragment?</Divider>
+      <Title level={3}>Преимущества использования Fragment?</Title>
       <Paragraph>
         <ol>
           <li>
             {' '}
-            <Paragraph>
-              Уменьшение размера реального DOM (нету ненужного div в разметке).
-            </Paragraph>
+            <Paragraph>Уменьшение размера реального DOM (нету ненужного div в разметке).</Paragraph>
           </li>
           <li>
             {' '}
             <Paragraph>
-              Оптимизирует процесс обновления реального DOM в процессе ЖЦ
-              приложения.
+              Оптимизирует процесс обновления реального DOM в процессе ЖЦ приложения.
             </Paragraph>
           </li>
         </ol>
       </Paragraph>
 
-      <Divider>Способы записи Fragment?</Divider>
+      <Title level={3}>Способы записи Fragment?</Title>
       <Paragraph>
         <ol>
           <li>
@@ -55,8 +50,7 @@ const Fragment = () => {
           </li>
           <CodeBlock code={fragment.short} />
           <Paragraph>
-            При данной записи нет возможности его стилизовать (задавать
-            атрибуты).
+            При данной записи нет возможности его стилизовать (задавать атрибуты).
           </Paragraph>
           <li>
             {' '}
@@ -68,8 +62,8 @@ const Fragment = () => {
         </ol>
       </Paragraph>
       <Paragraph>
-        При данной записи появился импорт из библиотеки React, но данный способ
-        позволяет стилизовать фрагмент (задавать атрибуты).
+        При данной записи появился импорт из библиотеки React, но данный способ позволяет
+        стилизовать фрагмент (задавать атрибуты).
       </Paragraph>
     </>
   );
