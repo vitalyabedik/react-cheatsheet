ffde08efdceee88f1b333af96fca99c0c1a2781a
Vitali Bedik <vitalya.bedik@gmail.com>
2023-07-07 22:07:13 +0300

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_properties.484ccc06.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_cheat_sheet=self.webpackChunkreact_cheat_sheet||[]).push([[81],{9164:function(e){function t(e){e.languages.properties={comment:/^[ \t]*[#!].*$/m,"attr-value":{pattern:/(^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?: *[=:] *| ))(?:\\(?:\r\n|[\s\S])|[^\\\r\n])+/m,lookbehind:!0},"attr-name":/^[ \t]*(?:\\(?:\r\n|[\s\S])|[^\\\s:=])+?(?= *[=:] *| )/m,punctuation:/[=:]/}}e.exports=t,t.displayName="properties",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_properties.484ccc06.chunk.js.map
