b9e02d3084eead1d6ce35666b02b3a19eb5b4aa0
Vitali Bedik <vitalya.bedik@gmail.com>
2023-11-08 03:56:17 +0300

    fix: add basename for route

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
+
 import App from './app/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/Redev.React.1-project.React-cheatsheet'>
         <App />
     </BrowserRouter>
 );
